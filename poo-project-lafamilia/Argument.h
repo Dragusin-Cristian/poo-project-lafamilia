e64dcae7165be78e46fc2d3a2e97cee3b6b65a81
Stefan Boros <stefanzingzing@yahoo.com>
2022-11-13 10:44:14 +0200

    -Finished implementation of Argument.h -Started Util.h file for "global" functions (general utilities)

--- poo-project-lafamilia/Argument.h
+++ poo-project-lafamilia/Argument.h
@@ -1,72 +1,43 @@
 #pragma once
 
-#include "Source.cpp" //This will be CHANGED to #include "KeyboardInput.h" after that file is implemented, I think
+#include "Util.h"
 #include <string>
-
-//This is a class for "global" functions (public static functions) that can be used as general utilities
-//in any place of the code. This shall be moved to a different header file later on.
-class Util {
-public:
-	static const std::string WHITESPACE;
-
-	//This is from stack overflow: https://stackoverflow.com/questions/18972258/index-of-nth-occurrence-of-the-string
-	static int nthOccurrence(const std::string& str, const std::string& findMe, int nth)
-	{
-		size_t  pos = 0;
-		int     cnt = 0;
-
-		while (cnt != nth)
-		{
-			pos += 1;
-			pos = str.find(findMe, pos);
-			if (pos == std::string::npos)
-				return -1;
-			cnt++;
-		}
-		return pos;
-	}
-
-	//This is from stack overflow: https://stackoverflow.com/questions/20303821/how-to-check-if-string-is-in-array-of-strings
-	bool in_array(string value, const char* const* array, int size)
-	{
-		for (int i = 0; i < size; i++)
-		{
-			if (value == array[i])
-			{
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	//This is copy-pasted from somewhere else: https://www.techiedelight.com/trim-string-cpp-remove-leading-trailing-spaces/
-	std::string ltrim(const std::string& s)
-	{
-		size_t start = s.find_first_not_of(WHITESPACE);
-		return (start == std::string::npos) ? "" : s.substr(start);
-	}
-
-	std::string rtrim(const std::string& s)
-	{
-		size_t end = s.find_last_not_of(WHITESPACE);
-		return (end == std::string::npos) ? "" : s.substr(0, end + 1);
-	}
-
-	std::string trim(const std::string& s) {
-		return Util::rtrim(Util::ltrim(s));
-	}
-
-};
-const std::string Util::WHITESPACE = " \n\r\t\f\v";
-
 
 
 //NOTE: This class is made to work for the arguments of CREATE TABLE. For other commands, we might need
 //to modify it, or create another class altogether
-class Argument:Util {
+class Argument {
 public:
-	
+	//INPUT: an argument of the form "columnName TYPE defaultValue", ex: "id INTEGER 1"
+	//This function sets the values for this class' private variables: columnName, type and defaultValue
+	Argument(std::string arguments)
+	{
+		arguments = this->removeExtraSpaces(arguments);
+		this->validateArgument(arguments);
+
+		int index_firstSpace = arguments.find(" ");
+		int index_secondSpace = Util::nthOccurrence(arguments, " ", 2);
+
+		this->columnName = arguments.substr(0, index_firstSpace);
+		this->type = arguments.substr(index_firstSpace + 1, index_secondSpace - index_firstSpace - 1);
+		this->defaultValue = arguments.substr(index_secondSpace + 1, arguments.size() - index_secondSpace);
+	}
+
+	//GETTERS:
+	std::string getColumnName()
+	{
+		return this->columnName;
+	}
+
+	std::string getType()
+	{
+		return this->type;
+	}
+
+	std::string getDefaultValue()
+	{
+		return this->defaultValue;
+	}
 
 private:
 	std::string columnName = "";
@@ -76,30 +47,22 @@ private:
 	static const char* const ALL_DATA_TYPES[];
 	static const int NUMBER_OF_TYPES = 5;
 
-	//INPUT: an argument of the form "columnName TYPE defaultValue", ex: "id INTEGER 1"
-	//This function sets the values for this class' private variables: columnName, type and defaultValue
-	Argument(const std::string arguments)
-	{
-		this->validateArgument(arguments);
-
-		this->columnName = arguments.substr(0, arguments.find(" "));
-		this->type = arguments.substr(Util::nthOccurrence(arguments, " ", 1), Util::nthOccurrence(arguments, " ", 2));
-		this->defaultValue = arguments.substr(Util::nthOccurrence(arguments, " ", 2), Util::nthOccurrence(arguments, " ", 3));
-	}
-
+	
 	//INPUT: an argument list of the form "columnName TYPE defaultValue", ex: "id INTEGER 1"
 	//OUTPUT: the same argument list, but in each place where there are two or more spaces separating the arguments, only one space is left
 	std::string removeExtraSpaces(std::string arguments)
 	{
 		//Removes spaces from beginning and end
-		arguments = Util::trim(arguments);
+		std::string output = Util::trim(arguments);
 
 		//Removes extra spaces between arguments
-		for (size_t i = 0; i < arguments.size() - 1; ++i) {
-			while (arguments[i] == ' ' && arguments[i + 1] == ' ') {
-				arguments.erase(i + 1, 1);
+		for (size_t i = 0; i < output.size() - 1; ++i) {
+			while (output[i] == ' ' && output[i + 1] == ' ') {
+				output.erase(i + 1, 1);
 			}
 		}
+
+		return output;
 	}
 
 	void validateArgument(std::string arguments)
@@ -109,33 +72,33 @@ private:
 		if (nrOfWords != 3)
 			throw "Invalid argument!"; //We will create an exception for this later in Exceptions.h
 
-		std::string columnName = arguments.substr(0, arguments.find(" "));
-		std::string type = arguments.substr(Util::nthOccurrence(arguments, " ", 1), Util::nthOccurrence(arguments, " ", 2));
-		std::string defaultValue = arguments.substr(Util::nthOccurrence(arguments, " ", 2), Util::nthOccurrence(arguments, " ", 3));
+		int index_firstSpace = arguments.find(" ");
+		int index_secondSpace = Util::nthOccurrence(arguments, " ", 2);
+
+		std::string columnName = arguments.substr(0, index_firstSpace);
+		std::string type = arguments.substr(index_firstSpace + 1, index_secondSpace - index_firstSpace - 1);
+		std::string defaultValue = arguments.substr(index_secondSpace + 1, arguments.size() - index_secondSpace);
 
 		//Check validity of type
-		if (!Util::in_array(type, this->ALL_DATA_TYPES, this->NUMBER_OF_TYPES)) {
+		if (Util::in_array(type, this->ALL_DATA_TYPES, this->NUMBER_OF_TYPES) == false) {
 			throw "Invalid argument type!"; //We will create an exception for this later in Exceptions.h
 		}
 
-		//Check validity of default value - TO IMPLEMENT...
+		//Check validity of default value
 		if (type == "INTEGER") {
-
-		}
-		else if (type == "TEXT") {
-
-		}
-		else if (type == "BLOB") {
-
+			for (size_t i = 0; i < defaultValue.size(); ++i)
+				if (!std::isdigit(defaultValue[i]))
+					throw "Invalid default value!"; //We will create an exception for this later in Exceptions.h
 		}
 		else if (type == "REAL") {
-
+			for (size_t i = 0; i < defaultValue.size(); ++i)
+				if (!std::isdigit(defaultValue[i]) && !defaultValue[i] == '.')
+					throw "Invalid default value!"; //We will create an exception for this later in Exceptions.h
 		}
-		else if (type == "NUMERIC") {
 
-		}
-
+		//We don't need validation for BLOB, TEXT and NUMERIC, since any of those 3 would be valid if it had gotten to this point
 	}
 };
 
+//STATIC CONST VARIABLE DEFINITIONS:
 const char* const Argument::ALL_DATA_TYPES[] = { "INTEGER", "TEXT", "BLOB", "REAL", "NUMERIC"};
---
+++ poo-project-lafamilia/Util.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <string>
+
+//This is a class for our "global" functions (public static functions) that can be used as general utilities
+//in any place of the code.
+class Util {
+public:
+	static const std::string WHITESPACE;
+
+	//This is from stack overflow: https://stackoverflow.com/questions/18972258/index-of-nth-occurrence-of-the-string
+	static int nthOccurrence(const std::string& str, const std::string& findMe, int nth)
+	{
+		size_t  pos = 0;
+		int     cnt = 0;
+
+		while (cnt != nth)
+		{
+			pos += 1;
+			pos = str.find(findMe, pos);
+			if (pos == std::string::npos)
+				return -1;
+			cnt++;
+		}
+		return pos;
+	}
+
+	//This is modified from stack overflow: https://stackoverflow.com/questions/20303821/how-to-check-if-string-is-in-array-of-strings
+	static bool in_array(std::string value, const char* const* array, int size)
+	{
+		for (int i = 0; i < size; i++){
+			if (value == array[i])
+				return true;
+		}
+
+		return false;
+	}
+
+	//These are copy-pasted from somewhere else: https://www.techiedelight.com/trim-string-cpp-remove-leading-trailing-spaces/
+	static std::string ltrim(const std::string& s)
+	{
+		size_t start = s.find_first_not_of(WHITESPACE);
+		return (start == std::string::npos) ? "" : s.substr(start);
+	}
+
+	static std::string rtrim(const std::string& s)
+	{
+		size_t end = s.find_last_not_of(WHITESPACE);
+		return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+	}
+
+	static std::string trim(const std::string& s) {
+		return Util::rtrim(Util::ltrim(s));
+	}
+
+};
+
+//STATIC CONST VARIABLE DEFINITIONS:
+const std::string Util::WHITESPACE = " \n\r\t\f\v";
