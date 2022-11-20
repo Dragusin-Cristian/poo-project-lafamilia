caad048c401d1714c2de6eb0800754b084718694
Cristian Dragusin <cristian.dragusin@microfocus.com>
2022-11-20 18:36:31 +0200

    Added condition class and handled most of select command

--- poo-project-lafamilia/Exceptions.h
+++ poo-project-lafamilia/Exceptions.h
@@ -7,7 +7,9 @@ enum ExceptionType {
 	INVALID_ARGUMENT_TYPE,
 	INVALID_DEFAULT_VALUE,
 	PARANTHESIS_MISMATCH,
-
+	NO_FIELDS_SELECTED,
+	INVALID_COMMAS,
+	FORBIDDEN_WORDS_USED,
 };
 
 class Exceptions : public std::exception {
@@ -27,6 +29,12 @@ public:
 			return "The argument you entered has an invalid type. Please type again:\n";
 		case INVALID_DEFAULT_VALUE:
 			return "The argument you entered has an invalid default value. Please type again:\n";
+		case NO_FIELDS_SELECTED:
+			return "No fields selected. Please type again:\n";
+		case INVALID_COMMAS:
+			return "Invalid commas. Please type again:\n";
+		case FORBIDDEN_WORDS_USED:
+			return "Cannot have fields or tables named with forbidden words. Please type again:\n"; // remains to implement
 		case PARANTHESIS_MISMATCH:
 			return "You missmatched paranthesis. Please type again:\n"; // remains to implement
 		default:
